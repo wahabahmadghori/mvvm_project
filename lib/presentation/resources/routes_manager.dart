61ca2f88e0b3dcf2ee4d3c56e3008c1d9393a36d
Wahab Ahmad <wahabahmadghori@gmail.com>
2022-05-26 14:01:12 +0500

    Lecture 15 - Start Adding Routes Manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,9 @@
+class RoutesManager {
+  static String splashRoute = '/splash';
+  static String onBoardingRoute = '/onboarding';
+  static String loginRoute = '/login';
+  static String registerRoute = '/register';
+  static String forgetPasswordRoute = '/forgetPassword';
+  static String mainRoute = '/main';
+  static String shopDetailsRoute = '/shopDetails';
+}
---
+++ lib/presentation/resources/theme_manager.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import 'values_manager.dart';
+import 'color_manager.dart';
+import 'font_manager.dart';
+import 'styles_manager.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    //main Colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    accentColor: ColorManager.grey,
+
+    //card view theme
+
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+    //App bar theme
+    appBarTheme: AppBarTheme(
+        color: ColorManager.primary,
+        centerTitle: true,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularTextStyle(
+            color: ColorManager.white, fontSize: FontSize.s16)),
+    //Button theme
+    buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.primaryOpacity70,
+        disabledColor: ColorManager.grey1),
+    //Elevated Button theme
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12)),
+      textStyle: getRegularTextStyle(color: ColorManager.white),
+      primary: ColorManager.primary,
+    )),
+
+    //Text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldTextStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      subtitle1: getMediumTextStyle(
+          color: ColorManager.lightGrey, fontSize: FontSize.s14),
+      caption: getRegularTextStyle(color: ColorManager.grey1),
+      bodyText1: getRegularTextStyle(color: ColorManager.grey),
+    ),
+
+    //input decoration theme(text from field)
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+      labelStyle: getSemiBoldTextStyle(color: ColorManager.darkGrey),
+      hintStyle: getRegularTextStyle(color: ColorManager.grey1),
+      errorStyle: getRegularTextStyle(color: ColorManager.error),
+      enabledBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(AppSize.s8),
+          ),
+          borderSide:
+              BorderSide(width: AppSize.s1_5, color: ColorManager.grey)),
+      errorBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(AppSize.s8),
+          ),
+          borderSide:
+              BorderSide(width: AppSize.s1_5, color: ColorManager.error)),
+      focusedBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(AppSize.s8),
+          ),
+          borderSide:
+              BorderSide(width: AppSize.s1_5, color: ColorManager.primary)),
+      focusedErrorBorder: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(AppSize.s8),
+          ),
+          borderSide:
+              BorderSide(width: AppSize.s1_5, color: ColorManager.primary)),
+    ),
+  );
+}
