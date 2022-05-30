1ca0ec467d92c4cc03098c63223c504ae1e0a126
FlutLab.io <>
2022-05-30 09:00:18 +0000

    Autosave

---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,5 @@
+const STRING_IMG_PATH = 'assets/images';
+
+class ImageAssets {
+  static String splashImage = '$STRING_IMG_PATH/splash_logo.png';
+}
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import '../resources/assets_manager.dart';
+import '../resources/color_manager.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: ColorManager.primary,
+        body: Center(
+          child: Image(image: AssetImage(ImageAssets.splashImage)),
+        ));
   }
 }
