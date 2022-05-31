1358de9d56224721c0da2a652d83a28439d7bff1
Wahab Ahmad <wahabahmadghori@gmail.com>
2022-05-31 15:15:27 +0500

    Lecture 25 - Adding Assets for Onboarding Screen

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,8 @@ const STRING_IMG_PATH = 'assets/images';
 
 class ImageAssets {
   static String splashImage = '$STRING_IMG_PATH/splash_logo.png';
+  static String onBoardingLogo1 = '$STRING_IMG_PATH/onboarding_logo1.svg';
+  static String onBoardingLogo2 = '$STRING_IMG_PATH/onboarding_logo2.svg';
+  static String onBoardingLogo3 = '$STRING_IMG_PATH/onboarding_logo3.svg';
+  static String onBoardingLogo4 = '$STRING_IMG_PATH/onboarding_logo4.svg';
 }
